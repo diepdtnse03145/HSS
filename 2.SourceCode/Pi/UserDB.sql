1ef638d48ccc6fb4489737a647ff66dbe0b4684a
Vo Tuan Hung <votuanhung1506@gmail.com>
2016-03-10 22:23:46 +0700

    Update User Database

--- 2.SourceCode/Pi/UserDB.sql
+++ 2.SourceCode/Pi/UserDB.sql
@@ -1,12 +1,13 @@
+-- Tao database voi 3 cot: ID, User, Password --
 Create table UserDB (ID int(11), User Char(64), Password Char(64) ) ;
-Insert into UserDB (id,user,password)
-values (1,'Tom Cat','1234');
+Insert into UserDB (id,user,password) -- add thong tin cua user vao database --
+values (1,'Tom Cat','1234'); -- id = 1, user = Tom Cat, password = 1234 --
 Insert into UserDB (id,user,password)
 values (2,'Hung','5678');
 Insert into UserDB (id,user,password)
 values (3,'Cuong','abcd');
-select * from UserDB;
-SELECT COUNT(User) FROM UserDB
+select * from UserDB; -- lay database ra output --
+SELECT COUNT(User) FROM UserDB -- dem so luong user co ten user la "Hung" --
 WHERE User="Hung";
 
 
