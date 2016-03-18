52c3a4fdb863cc33040441ff245717c806cea106
Vo Tuan Hung <votuanhung1506@gmail.com>
2016-03-18 13:42:27 +0700

    Add camera database

---
+++ 2.SourceCode/Pi/CameraDB.sql
@@ -0,0 +1,4 @@
+Create table CameraDB (Name text(1000), Url text(1000)) ; /*create database*/
+Insert into CameraDB (name, url)				/*add data to database*/
+values ('Camera1','www.asdf.com');
+select * from CameraDB; /*show database/*
