3421c2941363f1e6bf70779d292cd62bcf3aa91a
DiepDTN <diepdtnse03145@gmail.com>
2016-04-05 21:56:59 +0700

    Merge HSS_Push

--- 2.SourceCode/Android/HSS/android/java/org/hss/hss/MyService.java
+++ 2.SourceCode/Android/HSS/android/java/org/hss/hss/MyService.java
@@ -13,6 +13,12 @@ import android.support.v4.app.NotificationCompat;
 import android.app.NotificationManager;
 import android.app.IntentService;
 import android.support.v4.app.TaskStackBuilder;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.os.Vibrator;
+import android.media.RingtoneManager;
+import android.media.Ringtone;
+import android.net.Uri;
 
 import com.pusher.client.Pusher;
 import com.pusher.client.channel.Channel;
@@ -23,6 +29,7 @@ import java.io.IOException;
 public class MyService extends IntentService {
     public Pusher pusher;
     private NotificationManager mNotificationManager;
+    static int nofId;
 
     public MyService() {
         super("MyService");
@@ -30,6 +37,7 @@ public class MyService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent workIntent) {
+        nofId = 0;
         pusher = new Pusher("0d2838d583ebb263b04c");
 
         Channel channel = pusher.subscribe("test_channel");
@@ -47,19 +55,31 @@ public class MyService extends IntentService {
 
     private void sendNotification(String msg) {
         mNotificationManager = (NotificationManager)
-                this.getSystemService(Context.NOTIFICATION_SERVICE);
+        this.getSystemService(Context.NOTIFICATION_SERVICE);
 
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, MyActivity.class), 0);
+              new Intent(this, MyActivity.class), 0);
 
         NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                                       .setSmallIcon(R.drawable.icon)
-                                       .setContentTitle(msg)
-                                       .setContentText("Touch to view detail.");
+              new NotificationCompat.Builder(this)
+                               .setSmallIcon(R.drawable.icon)
+                               .setContentTitle(msg)
+                               .setContentText("Touch to view detail.");
         mBuilder.setContentIntent(contentIntent);
         mBuilder.setAutoCancel(true);
-        mNotificationManager.notify(1, mBuilder.build());
+
+        PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+        WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+        wl.acquire(15000);
+        Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(2000);
+         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+         Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+         r.play();
+
+        mNotificationManager.notify(nofId, mBuilder.build());
+        nofId++;
+
     }
 
 }
